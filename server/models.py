7e987e91be28752dda97c34a051f2bd49cbb5737
Teklu Mezgebo <mezge002@umn.edu>
2024-01-31 22:46:22 -0600

    model attribute validations created using SQLAlchemy

--- server/models.py
+++ server/models.py
@@ -11,8 +11,19 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
-
+    @validates('name')
+    def name_present(self, key, name):
+        if len(name) == 0:
+            raise ValueError("Name must be present")
+        elif Author.query.filter_by(name=name).first():
+            raise ValueError("Name already exists")
+        return name
+    
+    @validates('phone_number')
+    def phone_number_length(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits long")
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,8 +38,25 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
-
-
+    @validates('content')
+    def content_length(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content must be at least 250 characters long")
+    
+    @validates('summary')
+    def summary_length(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Summary must be less than 250 characters")
+    
+    @validates('category')
+    def category_type(self, key, category):
+        if category != 'Fiction' or category != 'Non-Fiction':
+            raise ValueError("Category must be Fiction or Non-Fiction")
+        
+    @validates('title')
+    def title_sufficiency(self, key, title):
+        if ("Won't Believe" or "Secret" or "Top" or "Guess") not in title:
+            raise ValueError("Title not sufficiently clickbait-y")
+    
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
